26140ac82e6735415f015d74cf5375090ef18555
alekskushchik <alkushchik@gmail.com>
2019-08-03 09:36:54 +0300

    Updates

---
+++ precache-manifest.72ba08389ab4af73776e10213b728716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b64b988f4148c560c4049dd3b34d5eb",
+    "url": "/movie_land/index.html"
+  },
+  {
+    "revision": "2402feed34b011628967",
+    "url": "/movie_land/static/css/main.56532eba.chunk.css"
+  },
+  {
+    "revision": "618d1d87cab67a2e8cf6",
+    "url": "/movie_land/static/js/2.2c4aef4a.chunk.js"
+  },
+  {
+    "revision": "2402feed34b011628967",
+    "url": "/movie_land/static/js/main.c6ad884e.chunk.js"
+  },
+  {
+    "revision": "cbf4bc1d03f598a85abf",
+    "url": "/movie_land/static/js/runtime~main.7868b5dd.js"
+  }
+]);
--- src/components/elements/NotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-    return (
-        <h1>Oops..! Something went wrong. This page doesn't exist.</h1>
-    )
-};
-
-export default NotFound;
